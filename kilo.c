3df7753895907e0ea7549da2754c7dc36b0890eb
Ray Chen <oldsharp@gmail.com>
2017-04-12 21:21:47 +0800

    Refactor keyboard input
    Make a function for low-level keypress reading, and another function
    for mapping keypresses to editor operations.

--- kilo.c
+++ kilo.c
@@ -50,22 +50,41 @@ void enable_raw_mode() {
 	}
 }
 
+char editor_read_key() {
+	int nread;
+	char c;
+
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) {
+			die("read");
+		}
+	}
+
+	return c;
+}
+
+void editor_process_keypress() {
+	char c = editor_read_key();
+
+	if (iscntrl(c)) {
+		printf("%d\r\n", c);
+	} else {
+		printf("%d ('%c')\r\n", c, c);
+	}
+
+	switch (c) {
+		case CTRL_KEY('q'):
+			exit(0);
+			break;
+	}
+}
+
 int main() {
 	enable_raw_mode();
 
 	while (1) {
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-			die("read");
-		}
-		if (iscntrl(c)) {
-			printf("%d\r\n", c);
-		} else {
-			printf("%d ('%c')\r\n", c, c);
-		}
-		if (c == CTRL_KEY('q')) {
-			break;
-		}
+		editor_process_keypress();
 	}
+
 	return 0;
 }
