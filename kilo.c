e09fb4c84982a428b5dfe3373f375d43d021302d
Ray Chen <oldsharp@gmail.com>
2017-04-24 21:06:45 +0800

    Allow to move the cursor using the w, a, s, d keys

--- kilo.c
+++ kilo.c
@@ -222,6 +222,24 @@ void editor_refresh_screen()
 	ab_free(&ab);
 }
 
+void editor_move_cursor(char key)
+{
+	switch (key) {
+		case 'a':
+			E.cx--;
+			break;
+		case 'd':
+			E.cx++;
+			break;
+		case 'w':
+			E.cy--;
+			break;
+		case 's':
+			E.cy++;
+			break;
+	}
+}
+
 void editor_process_keypress()
 {
 	char c = editor_read_key();
@@ -231,6 +249,12 @@ void editor_process_keypress()
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+		case 'w':
+		case 's':
+		case 'a':
+		case 'd':
+			editor_move_cursor(c);
 			break;
 	}
 }
