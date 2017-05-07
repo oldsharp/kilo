ec99dec2fee49bac916a06e45f4999a72cc50ae3
Ray Chen <oldsharp@gmail.com>
2017-05-07 21:42:48 +0800

    Make E.row a dynamically allocated array of erow

--- kilo.c
+++ kilo.c
@@ -59,7 +59,7 @@ struct editor_config {
 	int screenrows;
 	int screencols;
 	int numrows;
-	erow row;
+	erow *row;
 	struct termios orig_termios;
 };
 
@@ -453,6 +453,7 @@ void init_editor()
 	E.cx = 0;
 	E.cy = 0;
 	E.numrows = 0;
+	E.row = NULL;
 
 	if (get_window_size(&E.screenrows, &E.screencols) == -1) {
 		die("get_window_size");
