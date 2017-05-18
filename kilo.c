cc3b51e37fdd6884e05257e6229ed6c3fe1273ae
Ray Chen <oldsharp@gmail.com>
2017-05-19 02:27:20 +0800

    Add render and its size for non-printable chars

--- kilo.c
+++ kilo.c
@@ -46,7 +46,9 @@ enum editor_key {
 
 typedef struct erow {
 	int size;
+	int rsize;
 	char *chars;
+	char *render;
 } erow;
 
 struct editor_config {
@@ -280,6 +282,10 @@ void editor_append_row(char *s, size_t len)
 	E.row[at].chars = malloc(len + 1);
 	memcpy(E.row[at].chars, s, len);
 	E.row[at].chars[len] = '\0';
+
+	E.row[at].rsize = 0;
+	E.row[at].render = NULL;
+
 	E.numrows++;
 }
 
